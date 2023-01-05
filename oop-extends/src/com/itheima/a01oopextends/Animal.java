57e62003f67147b331f2310cd4973a8fa4d25923
liuxinxin <1761995468@qq.com>
2023-01-05 15:43:31 +0800

    继承练习：添加注意事项

--- oop-extends/src/com/itheima/a01oopextends/Animal.java
+++ oop-extends/src/com/itheima/a01oopextends/Animal.java
@@ -2,6 +2,11 @@ package com.itheima.a01oopextends;
 
 public class Animal {
 
+    //权限修饰符
+    //private 子类就无法访问了
+    //私有 只能在本类中访问
+    //注意事项：
+    //子类只能访问父类中非私有的成员
     public void eat() {
         System.out.println("吃东西");
     }
